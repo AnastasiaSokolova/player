c2f2d3e55eac9ee5cd2c54416a4379cb54c2037f
Nastya Sokolova <nas92sokol@gmail.com>
2017-01-16 01:32:01 +0200

    edit

--- Player/src/app/account/account.component.ts
+++ Player/src/app/account/account.component.ts
@@ -1,22 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../share/auth.service';
 import { User } from '../share/User';
+import { FieldsService } from '../forms/fields.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.scss']
+  styleUrls: ['./account.component.scss'],
+  providers:  [FieldsService]
 })
 export class AccountComponent implements OnInit {
 
   public user: User;
   public bool: boolean = false;
+  fields: any[];
 
-  constructor(private authService: AuthService) { }
+  constructor(private service: FieldsService, private authService: AuthService) {
+    this.authService.getUser(localStorage.getItem('username')).subscribe((res)=> {  
+       this.fields = this.service.getFieldsForEdit(res);
+       this.user = res;
+     })
+   }
+  
+
 
   ngOnInit() {
   	
-  	this.authService.getUser(localStorage.getItem('username')).subscribe((res)=> { this.user = res;})
+  	
+    //this.fields = this.service.getFieldsForEdit(this.user);
+    //console.log(this.fields);
 
   }
 
@@ -26,3 +38,29 @@ export class AccountComponent implements OnInit {
 
 
 }
+
+
+/*
+<ul [hidden]="bool">
+  <li>First name: {{user.firstname}}</li>
+  <li>Last name: {{user.lastname}}</li>
+  <li>Username: {{user.username}}</li>
+  <li>Password: {{user.password}}</li>
+  <li>Gender: {{user.gender}}</li>
+  <li>Email: {{user.email}}</li>
+ </ul>
+
+  <ul [hidden]='!bool'>
+  <li>First name: <input value={{user.firstname}}></li>
+  <li>Last name: <input value={{user.lastname}}></li>
+  <li>Username: <input value={{user.username}}></li>
+  <li>Password: <input value={{user.password}}></li>
+  <li>Gender:
+   <select>
+    <option value="male">Male</option>
+    <option value="female">Female</option>
+   </select>
+  </li>
+  <li>Email: <input value={{user.email}}></li>
+  </ul>
+*/
--- Player/src/app/forms/fields.service.ts
+++ Player/src/app/forms/fields.service.ts
@@ -82,4 +82,59 @@ export class FieldsService {
     return fields.sort((a, b) => a.order - b.order);
   }
 
+  getFieldsForEdit(user) {
+     let fields: FieldsBase<any>[] = [
+      new TextboxFields({
+        key: 'firstname',
+        label: 'First name',
+        value: user.firstname,
+        type: 'text',
+        required: true,
+        order: 1
+      }),
+      new TextboxFields({
+        key: 'lastname',
+        label: 'Last name',
+        value: user.lastname,
+        type: 'text',
+        required: true,
+        order: 2
+      }),
+      new TextboxFields({
+        key: 'username',
+        label: 'Username',
+        value: user.username,
+        type: "text",
+        required: true,
+        order: 3
+      }),
+      new TextboxFields({
+        key: 'password',
+        label: 'Password',
+        type: 'password',
+        value: user.password,
+        required: true,
+        order: 4
+      }),
+      new SelectFields({
+        key: 'gender',
+        label: 'Gender',
+        options: [
+          {key: 'male',  value: 'Male'},
+          {key: 'female',  value: 'Female'},
+        ],
+        order: 5
+      }),
+      new TextboxFields({
+        key: 'email',
+        label: 'Email',
+        type: 'email',
+        value: user.email,
+        order: 6
+      })
+
+    ];
+    return fields.sort((a, b) => a.order - b.order);
+  }
+
 }
